ec456748957cbee92e18ff6310cfbe939dd7a7aa
sunlubo <sunlubo.sun@gmail.com>
2020-10-28 14:24:58 +0800

    fix compile

--- Sources/Client/main.swift
+++ Sources/Client/main.swift
@@ -20,8 +20,8 @@ func main() throws {
   let client = try STUNClient.connect(to: "stun:stun.l.google.com:19302", configuration: config)
 
   var request = STUNMessage(type: .bindingRequest)
-  request.append(.init(type: .software, value: .string("swift-webrtc")))
-  request.append(.fingerprint())
+  request.appendAttribute(type: .software, value: "swift-webrtc")
+  request.appendFingerprint()
   client.send(request) { result in
     switch result {
     case .success(let response):
